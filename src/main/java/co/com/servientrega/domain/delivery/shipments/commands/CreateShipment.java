308c3204d2d9fd8bd19f937f9d72b2fea36f39ba
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-19 20:07:04 -0500

    Define CreateShipment command

---
+++ src/main/java/co/com/servientrega/domain/delivery/shipments/commands/CreateShipment.java
@@ -0,0 +1,40 @@
+package co.com.servientrega.domain.delivery.shipments.commands;
+
+import co.com.servientrega.domain.delivery.shipments.values.*;
+import co.com.sofka.domain.generic.Command;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CreateShipment extends Command {
+    //todo private final Manager issuedBy;
+    private final Sender sender;
+    private final Addressee addressee;
+    private final PackageName packageName;
+    private final PackageDescription packageDescription;
+    private final Weight packageWeight;
+    private final Size packageSize;
+
+    public Sender sender() {
+        return sender;
+    }
+
+    public Addressee addressee() {
+        return addressee;
+    }
+
+    public PackageName packageName() {
+        return packageName;
+    }
+
+    public PackageDescription packageDescription() {
+        return packageDescription;
+    }
+
+    public Weight packageWeight() {
+        return packageWeight;
+    }
+
+    public Size packageSize() {
+        return packageSize;
+    }
+}
