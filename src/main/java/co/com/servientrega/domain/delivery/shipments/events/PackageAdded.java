d1ce2f307d12dc8ce377e91bc639ac751989269e
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 08:35:29 -0500

    Update: add field of type ShipmentId on PackageAdded

--- src/main/java/co/com/servientrega/domain/delivery/shipments/events/PackageAdded.java
+++ src/main/java/co/com/servientrega/domain/delivery/shipments/events/PackageAdded.java
@@ -1,6 +1,7 @@
 package co.com.servientrega.domain.delivery.shipments.events;
 
 import co.com.servientrega.domain.delivery.shipments.identity.PackageId;
+import co.com.servientrega.domain.delivery.shipments.identity.ShipmentId;
 import co.com.servientrega.domain.delivery.shipments.values.PackageDescription;
 import co.com.servientrega.domain.delivery.shipments.values.PackageName;
 import co.com.servientrega.domain.delivery.shipments.values.Size;
@@ -8,19 +9,25 @@ import co.com.servientrega.domain.delivery.shipments.values.Weight;
 import co.com.sofka.domain.generic.DomainEvent;
 
 public class PackageAdded extends DomainEvent {
+    private final ShipmentId shipmentId;
     private final PackageId packageId;
     private final PackageName packageName;
     private final PackageDescription packageDescription;
     private final Weight packageWeight;
     private final Size packageSize;
 
-    public PackageAdded(PackageId packageId, PackageName packageName, PackageDescription packageDescription, Weight packageWeight, Size packageSize) {
+    public PackageAdded(ShipmentId shipmentId, PackageId packageId, PackageName packageName, PackageDescription packageDescription, Weight packageWeight, Size packageSize) {
         super("Servientrega.shipments.PackageAdded");
+        this.shipmentId = shipmentId;
         this.packageId = packageId;
         this.packageName = packageName;
         this.packageDescription = packageDescription;
         this.packageWeight = packageWeight;
         this.packageSize = packageSize;
+    }
+
+    public ShipmentId shipmentId() {
+        return shipmentId;
     }
 
     public PackageId packageId() {
