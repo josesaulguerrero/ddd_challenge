7fa389668aea565c9e5d0bb2c3e3552054ce9858
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 13:55:41 -0500

    Fix: add location field on CreatedOffice event

--- src/main/java/co/com/servientrega/domain/delivery/offices/events/CreatedOffice.java
+++ src/main/java/co/com/servientrega/domain/delivery/offices/events/CreatedOffice.java
@@ -1,17 +1,20 @@
 package co.com.servientrega.domain.delivery.offices.events;
 
 import co.com.servientrega.domain.delivery.common.values.Date;
+import co.com.servientrega.domain.delivery.common.values.Location;
 import co.com.servientrega.domain.delivery.offices.identity.OfficeId;
 import co.com.sofka.domain.generic.DomainEvent;
 
 public class CreatedOffice extends DomainEvent {
     private final OfficeId officeId;
     private final Date createdAt;
+    private final Location location;
 
-    public CreatedOffice(OfficeId officeId, Date createdAt) {
+    public CreatedOffice(OfficeId officeId, Date createdAt, Location location) {
         super("Servientrega.offices.CreatedOffice");
         this.officeId = officeId;
         this.createdAt = createdAt;
+        this.location = location;
     }
 
     public OfficeId officeId() {
@@ -20,5 +23,9 @@ public class CreatedOffice extends DomainEvent {
 
     public Date createdAt() {
         return createdAt;
+    }
+
+    public Location location() {
+        return location;
     }
 }
