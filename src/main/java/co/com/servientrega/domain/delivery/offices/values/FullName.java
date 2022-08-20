9cf7ed8e24963fb6f5b4d955791efba0272156a3
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 10:30:44 -0500

    Define FullName vavlue object

---
+++ src/main/java/co/com/servientrega/domain/delivery/offices/values/FullName.java
@@ -0,0 +1,30 @@
+package co.com.servientrega.domain.delivery.offices.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class FullName implements ValueObject<FullName.Props> {
+    private String name;
+    private String lastName;
+
+    public interface Props {
+        String name();
+        String lastName();
+    }
+
+    @Override
+    public Props value() {
+        return new Props() {
+            @Override
+            public String name() {
+                return name;
+            }
+
+            @Override
+            public String lastName() {
+                return lastName;
+            }
+        };
+    }
+}
