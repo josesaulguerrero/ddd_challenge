2d594e3fb3861062c40f76c3a7acec7f0388b612
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 16:57:31 -0500

    Update: add equals and hashCode to TransportId

--- src/main/java/co/com/servientrega/domain/delivery/transports/identity/TransportId.java
+++ src/main/java/co/com/servientrega/domain/delivery/transports/identity/TransportId.java
@@ -5,6 +5,8 @@ import co.com.servientrega.infrastructure.id.strategies.UUIDGenerationStrategy;
 import co.com.sofka.domain.generic.Identity;
 import lombok.AllArgsConstructor;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 public class TransportId extends Identity {
     private static final IdGenerationStrategy<String> idGenerationStrategy = new UUIDGenerationStrategy();
@@ -12,5 +14,19 @@ public class TransportId extends Identity {
 
     public TransportId() {
         this.value = idGenerationStrategy.generate();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        TransportId that = (TransportId) o;
+        return value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), value);
     }
 }
