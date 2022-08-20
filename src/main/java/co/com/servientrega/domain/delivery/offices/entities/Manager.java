3d179248cfc02b9b943ce4878fea87d271726857
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 13:47:52 -0500

    Removed Location field from Manager

--- src/main/java/co/com/servientrega/domain/delivery/offices/entities/Manager.java
+++ src/main/java/co/com/servientrega/domain/delivery/offices/entities/Manager.java
@@ -5,7 +5,7 @@ import co.com.servientrega.domain.delivery.common.identity.EmployeeId;
 import co.com.servientrega.domain.delivery.common.values.*;
 
 public class Manager extends Employee {
-    public Manager(EmployeeId entityId, DNI dni, FullName fullName, Location location, Email email, PhoneNumber phoneNumber, Salary salary) {
-        super(entityId, dni, fullName, location, email, phoneNumber, salary);
+    public Manager(EmployeeId entityId, DNI dni, FullName fullName, Email email, PhoneNumber phoneNumber, Salary salary) {
+        super(entityId, dni, fullName, email, phoneNumber, salary);
     }
 }
