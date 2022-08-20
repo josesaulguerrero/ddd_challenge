c36bd7a433e160a0d94e978386d38529d968624d
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-19 20:47:15 -0500

    Define ShipmentCreate event

---
+++ src/main/java/co/com/servientrega/domain/delivery/shipments/events/ShipmentCreated.java
@@ -0,0 +1,17 @@
+package co.com.servientrega.domain.delivery.shipments.events;
+
+import co.com.servientrega.domain.delivery.shipments.identity.ShipmentId;
+import co.com.sofka.domain.generic.DomainEvent;
+
+public class ShipmentCreated extends DomainEvent {
+    private final ShipmentId shipmentId;
+
+    public ShipmentCreated(ShipmentId shipmentId) {
+        super("Servientrega.domain.ShipmentCreated");
+        this.shipmentId = shipmentId;
+    }
+
+    public ShipmentId shipmentId() {
+        return shipmentId;
+    }
+}
