067f5f7db9aee7c6474a2dbf667457d666cec329
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-19 21:21:42 -0500

    Define Invoice entity

---
+++ src/main/java/co/com/servientrega/domain/delivery/shipments/entities/Invoice.java
@@ -0,0 +1,23 @@
+package co.com.servientrega.domain.delivery.shipments.entities;
+
+import co.com.servientrega.domain.delivery.shipments.identity.InvoiceId;
+import co.com.servientrega.domain.delivery.shipments.values.Money;
+import co.com.sofka.domain.generic.Entity;
+
+public class Invoice extends Entity<InvoiceId> {
+    //todo private final Manager issuedBy;
+    private Money cost;
+
+    public Invoice(InvoiceId entityId, Money cost) {
+        super(entityId);
+        this.cost = cost;
+    }
+
+    public Invoice(Money cost) {
+        this(new InvoiceId(), cost);
+    }
+
+    public Money cost() {
+        return cost;
+    }
+}
