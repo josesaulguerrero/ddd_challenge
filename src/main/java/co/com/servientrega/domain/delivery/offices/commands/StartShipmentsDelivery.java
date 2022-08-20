3cf28c9383c92dab460d2c25373a30748fbf6b67
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 11:32:34 -0500

    Define StartShipmentDelivery command

---
+++ src/main/java/co/com/servientrega/domain/delivery/offices/commands/StartShipmentsDelivery.java
@@ -0,0 +1,21 @@
+package co.com.servientrega.domain.delivery.offices.commands;
+
+import co.com.servientrega.domain.delivery.offices.identity.OfficeId;
+import co.com.servientrega.domain.delivery.offices.identity.StoreId;
+import co.com.sofka.domain.generic.Command;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class StartShipmentsDelivery extends Command {
+    //todo private final TransportId transportId;
+    private final OfficeId officeId;
+    private final StoreId storeId;
+
+    public OfficeId officeId() {
+        return officeId;
+    }
+
+    public StoreId storeId() {
+        return storeId;
+    }
+}
