ce33b6c13403f61afb789b880bbd54ae6be31f1b
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 14:46:06 -0500

    Refactor: add newManagerId field on ManagerChanged event

--- src/main/java/co/com/servientrega/domain/delivery/offices/events/ManagerChanged.java
+++ src/main/java/co/com/servientrega/domain/delivery/offices/events/ManagerChanged.java
@@ -7,11 +7,13 @@ import co.com.sofka.domain.generic.DomainEvent;
 public class ManagerChanged extends DomainEvent {
     private final OfficeId officeId;
     private final EmployeeId previousManagerId;
+    private final EmployeeId newManagerId;
 
-    public ManagerChanged(OfficeId officeId, EmployeeId previousManagerId) {
+    public ManagerChanged(OfficeId officeId, EmployeeId previousManagerId, EmployeeId newManagerId) {
         super("Servientrega.offices.ManagerChanged");
         this.officeId = officeId;
         this.previousManagerId = previousManagerId;
+        this.newManagerId = newManagerId;
     }
 
     public OfficeId officeId() {
@@ -20,5 +22,9 @@ public class ManagerChanged extends DomainEvent {
 
     public EmployeeId previousManagerId() {
         return previousManagerId;
+    }
+
+    public EmployeeId newManagerId() {
+        return newManagerId;
     }
 }
