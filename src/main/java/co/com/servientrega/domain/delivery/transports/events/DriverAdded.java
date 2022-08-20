ea3a82c1e3b503da273621f81254f9f1e4f7250f
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 18:14:15 -0500

    Add DriverAdded and DeliveryAssistantAdded events

---
+++ src/main/java/co/com/servientrega/domain/delivery/transports/events/DriverAdded.java
@@ -0,0 +1,41 @@
+package co.com.servientrega.domain.delivery.transports.events;
+
+import co.com.servientrega.domain.delivery.common.values.*;
+import co.com.sofka.domain.generic.DomainEvent;
+
+public class DriverAdded extends DomainEvent {
+    private final DNI dni;
+    private final FullName fullName;
+    private final Email email;
+    private final PhoneNumber phoneNumber;
+    private final Salary salary;
+
+    public DriverAdded(DNI dni, FullName fullName, Email email, PhoneNumber phoneNumber, Salary salary) {
+        super("Servientrega.transports.DriverAdded");
+        this.dni = dni;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+    }
+
+    public DNI dni() {
+        return dni;
+    }
+
+    public FullName fullName() {
+        return fullName;
+    }
+
+    public Email email() {
+        return email;
+    }
+
+    public PhoneNumber phoneNumber() {
+        return phoneNumber;
+    }
+
+    public Salary salary() {
+        return salary;
+    }
+}
