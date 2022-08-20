b01bb3a0feafb05b4115dea036638f46dbb86973
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-19 20:11:16 -0500

    Define CalculateDeliveryPrice command

---
+++ src/main/java/co/com/servientrega/domain/delivery/shipments/commands/CalculateDeliveryPrice.java
@@ -0,0 +1,26 @@
+package co.com.servientrega.domain.delivery.shipments.commands;
+
+import co.com.servientrega.domain.delivery.shipments.identity.ShipmentId;
+import co.com.servientrega.domain.delivery.shipments.values.Size;
+import co.com.servientrega.domain.delivery.shipments.values.Weight;
+import co.com.sofka.domain.generic.Command;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CalculateDeliveryPrice extends Command {
+    private final ShipmentId shipmentId;
+    private final Size packageSize;
+    private final Weight packageWeight;
+
+    public ShipmentId shipmentId() {
+        return shipmentId;
+    }
+
+    public Size packageSize() {
+        return packageSize;
+    }
+
+    public Weight packageWeight() {
+        return packageWeight;
+    }
+}
