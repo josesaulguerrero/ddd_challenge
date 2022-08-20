c1b3eb6859b25b3c03c6d8c5f877996895ad380b
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 14:00:21 -0500

    Define Store entity

---
+++ src/main/java/co/com/servientrega/domain/delivery/offices/entities/Store.java
@@ -0,0 +1,28 @@
+package co.com.servientrega.domain.delivery.offices.entities;
+
+import co.com.servientrega.domain.delivery.common.values.Size;
+import co.com.servientrega.domain.delivery.offices.identity.StoreId;
+import co.com.servientrega.domain.delivery.shipments.identity.ShipmentId;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Store extends Entity<StoreId> {
+    private final Size capacity;
+    private final Set<ShipmentId> shipmentsIds;
+
+    public Store(StoreId entityId, Size capacity) {
+        super(entityId);
+        this.capacity = capacity;
+        this.shipmentsIds = new HashSet<>();
+    }
+
+    public Size capacity() {
+        return capacity;
+    }
+
+    public Set<ShipmentId> shipmentsIds() {
+        return shipmentsIds;
+    }
+}
