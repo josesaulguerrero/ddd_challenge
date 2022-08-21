8274a8281ce844b95c0e5a87c71bb82fe9f3f287
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 21:37:17 -0500

    Refactor: Update use case to make it work dynamically

--- src/main/java/co/com/servientrega/domain/usecases/AddDeliveryOrderUseCase.java
+++ src/main/java/co/com/servientrega/domain/usecases/AddDeliveryOrderUseCase.java
@@ -4,8 +4,6 @@ import co.com.servientrega.domain.delivery.shipments.commands.AddDeliveryOrder;
 import co.com.servientrega.domain.delivery.shipments.entities.DeliveryOrder;
 import co.com.servientrega.domain.delivery.shipments.entities.root.Shipment;
 import co.com.servientrega.domain.delivery.shipments.identity.ShipmentId;
-import co.com.servientrega.domain.delivery.shipments.values.Addressee;
-import co.com.servientrega.domain.delivery.shipments.values.Sender;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
@@ -16,10 +14,8 @@ public class AddDeliveryOrderUseCase extends UseCase<RequestCommand<AddDeliveryO
         AddDeliveryOrder command = addDeliveryOrderRequestCommand.getCommand();
         ShipmentId shipmentId = command.shipmentId();
         Shipment shipment = Shipment.from(shipmentId, super.retrieveEvents(shipmentId.value()));
-        Sender sender = new Sender("Pepito Perez", "3204545786", "93247983274", "Street 34 # 3 - 46");
-        Addressee addressee = new Addressee("Juanito Gonzalez", "3098540568", "345345435234", "Street 56 # 1 - 34");
-        DeliveryOrder deliveryOrder = new DeliveryOrder(sender, addressee);
-        shipment.addDeliveryOrder(shipmentId, deliveryOrder.identity(), sender, addressee);
+        DeliveryOrder deliveryOrder = new DeliveryOrder(command.sender(), command.addressee());
+        shipment.addDeliveryOrder(shipmentId, deliveryOrder.identity(), command.sender(), command.addressee());
 
         super.emit().onResponse(new ResponseEvents(shipment.getUncommittedChanges()));
     }
