b4f7affedc68e30e79ad68e1e5b095bb2b97c39e
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 23:03:33 -0500

    Update: add field with aggregate root id on AddManager command

--- src/main/java/co/com/servientrega/domain/delivery/offices/commands/AddManager.java
+++ src/main/java/co/com/servientrega/domain/delivery/offices/commands/AddManager.java
@@ -1,16 +1,22 @@
 package co.com.servientrega.domain.delivery.offices.commands;
 
 import co.com.servientrega.domain.delivery.common.values.*;
+import co.com.servientrega.domain.delivery.offices.identity.OfficeId;
 import co.com.sofka.domain.generic.Command;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class AddManager extends Command {
+    private final OfficeId officeId;
     private final DNI dni;
     private final FullName fullName;
     private final Email email;
     private final PhoneNumber phoneNumber;
     private final Salary salary;
+
+    public OfficeId officeId() {
+        return officeId;
+    }
 
     public DNI dni() {
         return dni;
