bc7e26f398099b07827e66fd0e9a551a97160bb7
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-21 01:12:40 -0500

    Update: add aggregate root id

--- src/main/java/co/com/servientrega/domain/delivery/transports/commands/AddDeliveryAssistant.java
+++ src/main/java/co/com/servientrega/domain/delivery/transports/commands/AddDeliveryAssistant.java
@@ -1,16 +1,23 @@
 package co.com.servientrega.domain.delivery.transports.commands;
 
 import co.com.servientrega.domain.delivery.common.values.*;
+import co.com.servientrega.domain.delivery.transports.entities.root.Transport;
+import co.com.servientrega.domain.delivery.transports.identity.TransportId;
 import co.com.sofka.domain.generic.Command;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class AddDeliveryAssistant extends Command {
+    private final TransportId transportId;
     private final DNI dni;
     private final FullName fullName;
     private final Email email;
     private final PhoneNumber phoneNumber;
     private final Salary salary;
+
+    public TransportId transportId() {
+        return transportId;
+    }
 
     public DNI dni() {
         return dni;
