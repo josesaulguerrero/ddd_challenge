adc7e4d1cb5609a9ac5c28acee7337170c506bf4
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-20 22:44:16 -0500

    Fix: AddPackageUseCaseTest wasn't extending from MockitoExtension

--- src/test/java/co/com/servientrega/domain/usecases/AddPackageUseCaseTest.java
+++ src/test/java/co/com/servientrega/domain/usecases/AddPackageUseCaseTest.java
@@ -19,14 +19,17 @@ import co.com.sofka.business.support.RequestCommand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class AddPackageUseCaseTest {
     private final String randomUUID = generateRandomUUID();
 
