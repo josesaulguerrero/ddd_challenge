c8d8ee6068706e888163e2d039a9e53078fdb9b9
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-21 01:07:01 -0500

    Add all-args constructor on TransportId

--- src/main/java/co/com/servientrega/domain/delivery/transports/identity/TransportId.java
+++ src/main/java/co/com/servientrega/domain/delivery/transports/identity/TransportId.java
@@ -3,17 +3,20 @@ package co.com.servientrega.domain.delivery.transports.identity;
 import co.com.servientrega.infrastructure.id.IdGenerationStrategy;
 import co.com.servientrega.infrastructure.id.strategies.UUIDGenerationStrategy;
 import co.com.sofka.domain.generic.Identity;
-import lombok.AllArgsConstructor;
 
 import java.util.Objects;
 
-@AllArgsConstructor
 public class TransportId extends Identity {
     private static final IdGenerationStrategy<String> idGenerationStrategy = new UUIDGenerationStrategy();
     private final String value;
 
     public TransportId() {
         this.value = idGenerationStrategy.generate();
+    }
+
+    public TransportId(String value) {
+        super(value);
+        this.value = value;
     }
 
     @Override
