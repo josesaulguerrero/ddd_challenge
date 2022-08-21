a664709479ceb3e698f318aee71fed324b31c963
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-21 00:16:46 -0500

    Refactor: remove unused attributes from CreateShipment command

--- src/main/java/co/com/servientrega/domain/delivery/shipments/commands/CreateShipment.java
+++ src/main/java/co/com/servientrega/domain/delivery/shipments/commands/CreateShipment.java
@@ -1,47 +1,14 @@
 package co.com.servientrega.domain.delivery.shipments.commands;
 
-import co.com.servientrega.domain.delivery.common.identity.EmployeeId;
-import co.com.servientrega.domain.delivery.common.values.Size;
-import co.com.servientrega.domain.delivery.common.values.Weight;
-import co.com.servientrega.domain.delivery.shipments.values.*;
+import co.com.servientrega.domain.delivery.common.values.Date;
 import co.com.sofka.domain.generic.Command;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class CreateShipment extends Command {
-    private final EmployeeId issuerId;
-    private final Sender sender;
-    private final Addressee addressee;
-    private final PackageName packageName;
-    private final PackageDescription packageDescription;
-    private final Weight packageWeight;
-    private final Size packageSize;
+    private final Date sentAt;
 
-    public EmployeeId issuerId() {
-        return issuerId;
-    }
-
-    public Sender sender() {
-        return sender;
-    }
-
-    public Addressee addressee() {
-        return addressee;
-    }
-
-    public PackageName packageName() {
-        return packageName;
-    }
-
-    public PackageDescription packageDescription() {
-        return packageDescription;
-    }
-
-    public Weight packageWeight() {
-        return packageWeight;
-    }
-
-    public Size packageSize() {
-        return packageSize;
+    public Date sentAt() {
+        return sentAt;
     }
 }
